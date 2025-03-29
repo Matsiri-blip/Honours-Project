cb388360251882030704a787473a7ba37b79c5d9
Matsiri-blip <2604158@students.wits.ac.za>
2025-03-29 18:36:14 +0200

    use all the functions defined to return graphs
    here i use the functions binary matrix, adjacency matrix, vertices and edges function to return a graph of any n with k

--- more_systematic_graphs.py
+++ more_systematic_graphs.py
@@ -63,3 +63,75 @@ def edges(n,k):
         plt.legend()
         plt.text(-0.035,0,f'{k}',fontsize = 10)
   return plt.show()
+
+
+
+
+
+
+##Putting everything together
+def Graph(n,k):
+  def binary_matrix(n,k):
+    m = (n**2-n)/2
+    binary = int(bin(k)[2:])
+    binary_vec = [int(d) for d in str(binary)]
+    l = len(binary_vec)
+    if m!=len(binary_vec):
+        horizontal_matrix = np.concatenate((np.zeros(int(m-l)),binary_vec))
+    else:
+        horizontal_matrix = np.array([int(i) for i in str(binary)])
+    ##this returns the binary matrix
+    return horizontal_matrix
+## put the vector into an adjacency matrix
+  def  adjacency_matrix(n,k):
+    ##conditions for the matrix
+    ##for putting vector into the matrix
+    ##first define the pattern in which it is going to be put numbers for the 
+    ##binary vector
+    pattern = [0]
+    difference = n-1
+    for _ in range(1, n): ##this loop is to return the list of the pattern
+        next_number = pattern[-1] + difference ##here if n=3, the next number should be 
+        pattern.append(next_number) ##putting each number into a list
+        difference = difference - 1 ## ensure that the difference increase like the matrix
+    ##initialize the matrices
+    A = np.zeros((n,n))
+    B = binary_matrix(n,k) 
+    ##this loops through the A-matrix and binary vector
+    ##putting each element on the matrix is as we defined our labeling
+    for i in range(n-1):
+      A[i][1+i:] = B[pattern[i]:pattern[i+1]]
+
+    for l in range(n):
+        for j in range(n):
+            A[j,l] = A[l,j]
+    return A
+    ###define a function for vertices
+  def vertices(n):
+    l =[]
+    m =[]
+    for i in range(n):
+      l.append(float(np.cos(-2*np.pi*i/n+np.pi))) 	             	##x = cos(2pi/n) 
+      m.append(float(np.sin(-2*np.pi*i/n+np.pi)))                ##y = sin(2pi/n) both divide by n for number of vertices
+    return l,m 				
+  x = vertices(n)[0]
+  y = vertices(n)[1]                                                              ##using the vertice function defined
+  for i in range(n):                                                              ##looping through the vertices
+    plt.scatter(x[i],y[i], label = f"{i+1}")                                      ##plotting the vertices using scatter
+    for j in range(n):                                                            ##looping through the vertices
+      if n <2:
+        return [0]
+      elif adjacency_matrix(n,k)[i][j]==1:                                          ## check if there is an edge between the edges using adjacency matrix defined                                               ## if there is an edge add the vertices into a list
+        plt.plot([x[i],x[j]],[y[i],y[j]],color = 'gray')  
+        plt.axis('off')
+        plt.legend()
+        plt.text(-0.035,0,f'{k}',fontsize = 10)
+      else:
+        plt.axis('off')
+        plt.legend()
+        plt.text(-0.035,0,f'{k}',fontsize = 10)
+  return plt.show(), print(adjacency_matrix(n,k)),print(binary_matrix(n,k))    ##this function returns binary vector, adjacency matrix and graph
+n=4
+m = (n**2-n)/2
+for i in range(int(m)):
+  Graph(n,i)
