2d6b22f05011765dce9e6360664f66fba75aa360
Matsiri-blip <2604158@students.wits.ac.za>
2025-08-30 17:41:22 +0200

    add: close figure on graph plots
    add close figure on graph plots to save memory

--- ramsey_number.py
+++ ramsey_number.py
@@ -56,7 +56,7 @@ def Graph(n,k):
   
   ax.text(0,0,f'{k}',fontsize = 20, horizontalalignment='center', verticalalignment='center')
   ax.axis('off')
-  return fig
+  return plt.close(fig)
 def save_graph(n, k, folder_path, r_s=None):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
@@ -113,7 +113,7 @@ def ramsey_numbers(s,r):
 
       if not clique_found:
         is_ramsey_number = False
-        print(f'{n} is not ramsey number R{(s,r)}')
+        print(f'{n} is not ramsey number R{(s,r)}, because graph {k} has no clique')
         print(Graph(n,k))
         save_graph(n,k,folder_path)
         
